99bf5b519ada577e361aa4a62bed6d3330f170a5
kok202 <kok202@naver.com>
2025-01-08 21:24:33 +0900

    underbar case to camel case

--- Runtime/WaitFor.cs
+++ Runtime/WaitFor.cs
@@ -6,7 +6,7 @@ namespace NeedrunGameUtils
     {
         public static WaitForFixedUpdate fixedUpdate = new WaitForFixedUpdate();
         public static WaitForEndOfFrame endOfFrame = new WaitForEndOfFrame();
-        public static WaitForSecondsRealtime real_1_Second = new WaitForSecondsRealtime(1);
-        public static WaitForSecondsRealtime real_half_Second = new WaitForSecondsRealtime(0.5f);
+        public static WaitForSecondsRealtime realOneSecond = new WaitForSecondsRealtime(1);
+        public static WaitForSecondsRealtime realHalfSecond = new WaitForSecondsRealtime(0.5f);
     }
 }
