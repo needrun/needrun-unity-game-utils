6d2f4aa883274c14f7162a5da2bc89d157d468d8
kok202 <kok202@naver.com>
2025-01-09 00:36:47 +0900

    instanceAssigned 기능 추가

--- Runtime/SingletonMonoBehaviour.cs
+++ Runtime/SingletonMonoBehaviour.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace NeedrunGameUtils
 {
     public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
     {
         public static T instance;
-
+        public static UnityEvent<T> instanceAssigned = new UnityEvent<T>();
         protected virtual void Awake()
         {
             if (instance == null)
             {
                 instance = (T)this;
+                instanceAssigned.Invoke(instance);
                 DontDestroyOnLoad(gameObject);
             }
             else if (instance != this)
