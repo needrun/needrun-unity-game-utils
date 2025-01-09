eeaffc51830126a9ff816fc65231a2b056806f3f
kok202 <kok202@naver.com>
2025-01-10 01:13:30 +0900

    EditorOnly 추가

---
+++ Runtime/EditorOnly.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EditorOnly : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (Application.isEditor == false)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (Application.isEditor == false)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
