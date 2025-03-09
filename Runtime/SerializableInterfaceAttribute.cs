ba6b322fd6f590d0a7a43dd708fcd2bb11e4fc02
kok202 <kok202@naver.com>
2025-03-09 19:47:45 +0900

    SerializableInterfaceDrawer 제거

--- Runtime/SerializableInterfaceAttribute.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-namespace NeedrunGameUtils
-{
-    [AttributeUsage(AttributeTargets.Field)]
-    public class SerializableInterfaceAttribute : PropertyAttribute { }
-}
